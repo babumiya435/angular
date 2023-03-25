c4380213123730f75e50265ae7562267175de792
Babu Miya Mohammad <moba0222@netcracker.com>
2023-03-25 15:15:33 +0530

    angular-test-25-03

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { PostsComponent } from './posts/posts/posts.component';
+import { PostsCommentsComponent } from './posts/posts-comments/posts-comments.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PostsComponent,
+    PostsCommentsComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
